410b4498ef8ab84ecfecc4389c7ca8632f1ece75
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-09-24 21:03:17 +0300

    --amend

--- src/ankamantatra/commands/list_questions.py
+++ src/ankamantatra/commands/list_questions.py
@@ -1,17 +1,21 @@
 import click
 
-from utils import *
+from ankamantatra.utils import *
 
 
 @click.command(name='list', help='List all available questions to play with.')
-@click.option('--category', '-c')
+@click.option('--category', '-c', help="Filter by TEXT")
 @click.option('--show-answer', '-sa', is_flag=True)
 @click.option('--show-category', '-sc', is_flag=True)
-@click.option('--category-only', is_flag=True)
+@click.option('--category-only', is_flag=True, help="Show only the categories and hide questions")
 def list_questions(category, show_answer, show_category, category_only):
 
-    def display_question(question, category):
-        click.echo(f'{question["question"]}')
+    def display_question(question, category, type):
+        try:
+            click.echo(
+                f'{question["question"]} ({type} in {question["options"]})')
+        except:
+            click.echo(f'{question["question"]} ({type})')
         if show_answer:
             click.echo(f'--> {question["answer"]}\n')
         if show_category:
@@ -27,13 +31,14 @@ def list_questions(category, show_answer, show_category, category_only):
         try:
             questions = get_questions_of(category)
         except KeyError:
-            click.echo(f'"{category}" is not a valid category')
+            click.echo(f'"{category}" is not a valid category.\n Type "ankamantatra list --category-only" to see all available categories.')
+            return
         for type, questions in questions.items():
             for question in questions:
-                display_question(question, category)
+                display_question(question, category, type)
     else:
         questions = get_all_questions()
         for category, type in questions.items():
             for type, questions in type.items():
                 for question in questions:
-                    display_question(question, category)
+                    display_question(question, category, type)
