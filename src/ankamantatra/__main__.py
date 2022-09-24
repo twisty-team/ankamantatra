1b73b94c2aca8a0542422bcf184f4e85c96de3b4
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-09-24 15:51:58 +0300

    Start implementing 'list' command

--- src/ankamantatra/__main__.py
+++ src/ankamantatra/__main__.py
@@ -1,16 +1,39 @@
 import click
+import json
+from utils import get_category_questions, get_questions_from_json
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 def play():
     pass
 
+
 @cli.command()
-def list():
-    pass
+@click.option('--category', '-c')
+@click.option('--show-answer', '-sa', is_flag=True)
+@click.option('--show-category', '-sc', is_flag=True)
+def list(category, show_answer, show_category):
+    if category:
+        try:
+            questions = get_category_questions(category)
+        except KeyError:
+            click.echo(f'"{category}" is not a valid category')
+    else:
+        questions = get_questions_from_json()
+
+    for type, question_list in questions.items():
+        for question in question_list:
+            if show_answer:
+                click.echo(f'{question["question"]}')
+                click.echo(f'--> {question["answer"]}\n')
+            else:
+                click.echo(f'{question["question"]}')
+
 
 if __name__ == "__main__":
     cli()
--- src/ankamantatra/utils.py
+++ src/ankamantatra/utils.py
@@ -1,5 +1,20 @@
+import json
+
+
 def get_questions_from_json():
     pass
 
+
 def get_categories_from_json():
-    pass
+    with open('data.json') as f:
+        data = json.load(f)
+        return list(data.keys())
+
+
+def get_category_questions(category: str) -> list:
+    with open('data.json') as f:
+        data = json.load(f)
+        try:
+            return data[category]
+        except KeyError:
+            raise KeyError(f'Category {category} not found')
